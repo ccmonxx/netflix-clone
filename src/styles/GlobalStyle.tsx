c986734e24ee3dccc8b259aea128baab3aea0630
chocomong <chocomonq@gmail.com>
2023-01-15 00:07:47 +0900

    Theme and GlobalStyle

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: ${(props) => props.theme.white.darker};
+    color: ${(props) => props.theme.black.darker};
+    font-family: 'Source Sans Pro', sans-serif;
+    font-weight: 300;
+    line-height: 1.2;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
